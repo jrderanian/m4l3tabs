38114642cd1d0159fe5a16183da1eb14fa677b7f
John Deranian <fun@scooby.cc>
2022-09-04 17:54:57 -0400

    Initial Commit

---
+++ M4L3Tabs/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  M4L3Tabs
+//
+//  Created by John Deranian on 9/4/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ M4L3Tabs/M4L3TabsApp.swift
@@ -0,0 +1,17 @@
+//
+//  M4L3TabsApp.swift
+//  M4L3Tabs
+//
+//  Created by John Deranian on 9/4/22.
+//
+
+import SwiftUI
+
+@main
+struct M4L3TabsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
